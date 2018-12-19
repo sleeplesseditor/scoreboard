9ce48a1784707eecf3c44f5df3435a77e04c2df2
sleeplesseditor <josh@bryson.me.uk>
2018-12-19 13:33:31 +0000

    getGameScore tests passed

--- src/scoreboard.js
+++ src/scoreboard.js
@@ -16,9 +16,22 @@ export function setScore(playerNumber, previousState) {
 export function getGameScore(gamePoints) {
 
   // ... do something with gamePoints ...
+  // 0 === 'love-all'
+
+  let currentScore = '';
+  let winner = '';
+
+  if (gamePoints.player1 === 0 && gamePoints.player2 === 0){
+    currentScore = 'love-all'
+  } else if (gamePoints.player1 === 1 && gamePoints.player2 === 0) {
+    currentScore = '15-love'
+  } else if (gamePoints.player1 === 4 && gamePoints.player2 === 0) {
+    currentScore = 'Game, player1';
+    winner = 'player1';
+  } 
 
   return {
-    scoreCall: null,
-    winningPlayer: null,
+    scoreCall: currentScore,
+    winningPlayer: winner,
   }
 }
